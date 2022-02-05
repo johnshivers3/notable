87d680a78d7fd537e20ec460967b1bc535574e10
John Shivers <johnshivers3@gmail.com>
2022-02-04 19:58:55 -0500

    urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name="index")
+    path('/',views.index, name="index"),
+    path('create',views.create_record, name="create"),
+    path('display-all',views.get_all_records, name="display-all"),
+    path('display',views.get_select_records, name="display"),
+    path('update',views.update_record, name="update"),
+    path('delete',views.delete_record, name="delete"),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -3,3 +3,18 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world!")
+
+def create_record(request):
+    return HttpResponse("create_record")
+
+def get_all_records(request):
+    return HttpResponse("get_all_records")
+
+def get_select_records(request):
+    return HttpResponse("get_select_records")
+
+def update_record(request):
+    return HttpResponse("update_record")
+
+def delete_record(request):
+    return HttpResponse("delete_record")
